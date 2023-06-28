6462a7c3ce355c378929aa42a911c5cd1410cf23
Tony <tonyjays@yahoo.com>
2023-06-28 23:49:09 +0100

    An Ecommerce Site on Fashion Items With African Root

---
+++ store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.store, name="store"),
+    path("products/", views.products, name="products"),
+    path("product/<int:product_id>", views.product, name="product"),
+     path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+    # path("login/", views.login_view, name="login"),
+    # path("logout/", views.logout_view, name="logout"),
+    # path("register/", views.register, name="register"),
+    path("ourStory/", views.ourStory, name="ourStory"),
+    path("contact/", views.contact, name="contact"),
+]
